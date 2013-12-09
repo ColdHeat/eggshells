38a4b82f572f45e180325afa3132c8dd2e83e9b4
CodeKevin <kevin@codekevin.com>
2013-12-09 15:47:29 -0500

    clean up a bit more

--- nasm.py
+++ nasm.py
@@ -27,11 +27,13 @@ def assemble(asm, mode="elf"):
         asm = asm.read()
         os.unlink(temp.name+'.elf')
         os.unlink(linkme.name)
+        os.unlink(dir+'\\link.o')
         os.unlink(temp.name)
         return asm
     except:
         os.unlink(temp.name+'.elf')
         os.unlink(linkme.name)
+        os.unlink(dir+'\\link.o')
         os.unlink(temp.name)
         return "assembly failed"
 
@@ -49,7 +51,7 @@ def disassemble(elf, mode=32):
         os.unlink(temp.name)
         return 'disassembly failed'
 
-#print disassemble('\x48\x31\xc0\x50\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\xb0\x3b\x48\x89\xe7\x48\x31\xf6\x48\x31\xd2\x0f\x05', 64)
+print disassemble('\x48\x31\xc0\x50\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\xb0\x3b\x48\x89\xe7\x48\x31\xf6\x48\x31\xd2\x0f\x05', 64)
 asm = '''
 BITS 64
 main:
